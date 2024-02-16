6f169c3308ce04881609ff2e9a443c1330a43933
Benjam <benjamin.kielinski@hotmail.fr>
2024-02-16 18:07:57 +0100

    Location updates every 5s

--- app/src/main/java/com/example/basiclocation/MainActivity.kt
+++ app/src/main/java/com/example/basiclocation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.basiclocation
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.IntentSender
 import android.location.Location
@@ -23,7 +24,9 @@ import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.LocationSettingsRequest
 import com.google.android.gms.location.LocationSettingsResponse
@@ -35,10 +38,16 @@ class MainActivity : ComponentActivity() {
 
     val REQUEST_CHECK_SETTINGS = 0x1
     private val TAG = "MainActivity"
+
+    private lateinit var context: Context
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        context = this
+
         setContent {
             BasicLocationTheme {
                 // A surface container using the 'background' color from the theme
@@ -62,7 +71,7 @@ class MainActivity : ComponentActivity() {
                     // Check location activée
                     createLocationRequest()
                 } else -> {
-                    Toast.makeText(this, "Vous devez autoriser la localisation pour pouvoir utiliser l'appli", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Autorisez la localisation, pour utiliser l'appli", Toast.LENGTH_LONG).show()
             }
             }
         }
@@ -70,14 +79,24 @@ class MainActivity : ComponentActivity() {
         locationPermissionRequest.launch(arrayOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION))
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                for (location in p0.locations) {
+                    Toast.makeText(context, "lon : " + location.longitude + ", lat : " + location.latitude, Toast.LENGTH_LONG).show()
+                    Log.e(TAG, "lon : " + location.longitude + ", lat : " + location.latitude)
+                }
+            }
+        }
     }
+
     private fun createLocationRequest() {
-        Log.e(TAG, "createLocationRequest")
+        Log.e(TAG, "Location Request Created")
         // Google Play Check
         if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this) != ConnectionResult.SUCCESS) {
-            Toast.makeText(this, "Vous devez installer Google Play, pour pouvoir utiliser cette appli", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Installez Google Play, pour utiliser l'appli", Toast.LENGTH_LONG).show()
         } else {
-            val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000).build()
+            locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000).build()
             val builder = LocationSettingsRequest.Builder()
                 .addLocationRequest(locationRequest)
             val client: SettingsClient = LocationServices.getSettingsClient(this)
@@ -86,6 +105,7 @@ class MainActivity : ComponentActivity() {
             task.addOnSuccessListener { locationSettingsResponse ->
                 Log.e(TAG, "Location Settings OK")
                 getLastLocation()
+                startLocationUpdates()
             }
 
             task.addOnFailureListener { exception ->
@@ -114,11 +134,11 @@ class MainActivity : ComponentActivity() {
                 Handler(Looper.getMainLooper()).postDelayed({
                     getLastLocation()
                     // et lancer la demande de localisation en temps réel
-                    // startLocationUpdates()
+                    startLocationUpdates()
                 }, 2000) // Attendre 1 seconde que les settings s'activent pour de bon avant de récupérer la localisation
             } else {
                 Log.e(TAG, "Localisation non activée par l'utilisateur.")
-                Toast.makeText(this, "Vous devez activer la localisation, pour pouvoir utiliser cette appli", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Activez la localisation, pour utiliser l'appli", Toast.LENGTH_LONG).show()
 
             }
         }
@@ -133,5 +153,12 @@ class MainActivity : ComponentActivity() {
             .addOnSuccessListener { location : Location? ->
                 Log.e(TAG, "Last location : " + location.toString())
             }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+            locationCallback,
+            Looper.getMainLooper())
     }
 }
