8b6916053a7a26153f3b6f2d2fc5079ee88f3bca
Benjam <benjamin.kielinski@hotmail.fr>
2024-02-16 17:33:31 +0100

    Amélioration du process : 1 Check / ask Permission 2 Check Google Play 3 Check location activated 4 Ask for location if not activated 5 Get location if all OK (after 1 sec)

--- app/src/main/java/com/example/basiclocation/MainActivity.kt
+++ app/src/main/java/com/example/basiclocation/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.basiclocation
 
 import android.Manifest
-import android.content.pm.PackageManager
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
 import android.location.Location
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -12,34 +17,25 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.app.ActivityCompat
 import com.example.basiclocation.ui.theme.BasicLocationTheme
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsResponse
+import com.google.android.gms.location.Priority
+import com.google.android.gms.location.SettingsClient
+import com.google.android.gms.tasks.Task
 
 class MainActivity : ComponentActivity() {
 
+    val REQUEST_CHECK_SETTINGS = 0x1
+    private val TAG = "MainActivity"
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private val locationPermissionRequest = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-        when {
-            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                // Precise location access granted.
-            }
-            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                // Only approximate location access granted.
-            } else -> {
-                // No location access granted.
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,54 +45,93 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ) {}
             }
         }
 
+        // Location Permission
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Check location activée
+                    createLocationRequest()
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Check location activée
+                    createLocationRequest()
+                } else -> {
+                    Toast.makeText(this, "Vous devez autoriser la localisation pour pouvoir utiliser l'appli", Toast.LENGTH_LONG).show()
+            }
+            }
+        }
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+    }
+    private fun createLocationRequest() {
+        Log.e(TAG, "createLocationRequest")
         // Google Play Check
         if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this) != ConnectionResult.SUCCESS) {
             Toast.makeText(this, "Vous devez installer Google Play, pour pouvoir utiliser cette appli", Toast.LENGTH_LONG).show()
-        }
+        } else {
+            val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000).build()
+            val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest)
+            val client: SettingsClient = LocationServices.getSettingsClient(this)
+            val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
 
+            task.addOnSuccessListener { locationSettingsResponse ->
+                Log.e(TAG, "Location Settings OK")
+                getLastLocation()
+            }
+
+            task.addOnFailureListener { exception ->
+                if (exception is ResolvableApiException){
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        Log.e(TAG, "Location Settings NOT OK")
+                        exception.startResolutionForResult(this@MainActivity,
+                            REQUEST_CHECK_SETTINGS)
+                    } catch (sendEx: IntentSender.SendIntentException) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == Activity.RESULT_OK) {
+                Log.e(TAG, "Localisation activée par l'utilisateur.")
+                Handler(Looper.getMainLooper()).postDelayed({
+                    getLastLocation()
+                    // et lancer la demande de localisation en temps réel
+                    // startLocationUpdates()
+                }, 2000) // Attendre 1 seconde que les settings s'activent pour de bon avant de récupérer la localisation
+            } else {
+                Log.e(TAG, "Localisation non activée par l'utilisateur.")
+                Toast.makeText(this, "Vous devez activer la localisation, pour pouvoir utiliser cette appli", Toast.LENGTH_LONG).show()
+
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getLastLocation() {
         // Get Last known Location
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // Location Permission
-            locationPermissionRequest.launch(arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION))
-            return
-        } else {
-            fusedLocationClient.lastLocation
-                .addOnSuccessListener { location : Location? ->
-                    Log.e("MainActivity", location.toString())
-                }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BasicLocationTheme {
-        Greeting("Android")
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                Log.e(TAG, "Last location : " + location.toString())
+            }
     }
 }
